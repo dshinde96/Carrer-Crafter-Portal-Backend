f25acbdc594073f30b464bfdd0b4df8f14d5b55c
dshinde <dnyaneshwarshinde33368@gmail.com>
2024-05-01 10:39:08 +0530

    Refactored

--- Routes/PlacementStat.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { AuthenticateUser, restrictTo } = require('../Middleware/Authentication');
-const { handleCompaniesVisited } = require('../Controllers/PlacementStatController');
-
-router.route('/CompaniesVisited').get(AuthenticateUser, handleCompaniesVisited);
-// router.get('/SelectedStu/:CompanyName',AuthenticateUser,handleCompanyStu);
-
-module.exports = router;
